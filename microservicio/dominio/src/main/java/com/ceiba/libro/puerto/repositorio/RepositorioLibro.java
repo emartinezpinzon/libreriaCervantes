60c07a6f91a82a19d646d6760f642a8a1aa46c41
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-21 21:44:39 -0500

    Crea definiciones de CUD para RepositorioLibro

--- microservicio/dominio/src/main/java/com/ceiba/libro/puerto/repositorio/RepositorioLibro.java
+++ microservicio/dominio/src/main/java/com/ceiba/libro/puerto/repositorio/RepositorioLibro.java
@@ -1,4 +1,36 @@
 package com.ceiba.libro.puerto.repositorio;
 
+import com.ceiba.libro.modelo.entidad.Libro;
+
 public interface RepositorioLibro {
+    /**
+     * Permite crear un libro
+     *
+     * @param libro
+     * @return el id generado
+     */
+    Long crear(Libro libro);
+
+    /**
+     * Permite actualizar un libro
+     *
+     * @param libro
+     */
+    void actualizar(Libro libro);
+
+    /**
+     * Permite eliminar un libro
+     *
+     * @param id
+     */
+    void eliminar(Long id);
+
+    /**
+     * Permite validar si existe un usuario con un nombre
+     *
+     * @param titulo
+     * @return
+     */
+    boolean existe(String titulo);
+
 }
