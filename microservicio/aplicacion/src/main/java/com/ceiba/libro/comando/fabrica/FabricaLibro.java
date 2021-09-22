141a5c4b0d67ec943ef1af26d8256474f87c7a5d
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-22 00:57:59 -0500

    FabricaLibro agregado

--- microservicio/aplicacion/src/main/java/com/ceiba/libro/comando/fabrica/FabricaLibro.java
+++ microservicio/aplicacion/src/main/java/com/ceiba/libro/comando/fabrica/FabricaLibro.java
@@ -1,4 +1,17 @@
 package com.ceiba.libro.comando.fabrica;
 
+import com.ceiba.libro.comando.ComandoLibro;
+import com.ceiba.libro.modelo.entidad.Libro;
+
 public class FabricaLibro {
+    public Libro crear(ComandoLibro comandoLibro) {
+        return new Libro(
+                comandoLibro.getId(),
+                comandoLibro.getTitulo(),
+                comandoLibro.getCategoria(),
+                comandoLibro.getDistribucion(),
+                comandoLibro.getDisponibles(),
+                comandoLibro.getPrecio()
+        );
+    }
 }
