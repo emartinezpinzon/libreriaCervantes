c9f6d1fdc19e6f70b52b99d8e628b522eb963d3e
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-22 13:07:07 -0500

    Agrega una lista de libros a la compra

--- microservicio/dominio/src/main/java/com/ceiba/compra/modelo/entidad/Compra.java
+++ microservicio/dominio/src/main/java/com/ceiba/compra/modelo/entidad/Compra.java
@@ -1,4 +1,16 @@
 package com.ceiba.compra.modelo.entidad;
 
+import com.ceiba.libro.modelo.entidad.Libro;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
 public class Compra {
+    private List<Libro> libros;
+
+    public Compra() {
+        this.libros = new ArrayList<>();
+    }
 }
