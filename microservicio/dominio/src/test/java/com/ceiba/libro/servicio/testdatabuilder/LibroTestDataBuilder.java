2dff4338914904d45731bc294640e284950dfec9
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-22 00:42:12 -0500

    LibroTestDataBuilder agregado

--- microservicio/dominio/src/test/java/com/ceiba/libro/servicio/testdatabuilder/LibroTestDataBuilder.java
+++ microservicio/dominio/src/test/java/com/ceiba/libro/servicio/testdatabuilder/LibroTestDataBuilder.java
@@ -1,4 +1,31 @@
 package com.ceiba.libro.servicio.testdatabuilder;
 
+import com.ceiba.libro.modelo.entidad.Libro;
+
 public class LibroTestDataBuilder {
+
+    private Long id;
+    private String titulo;
+    private String categoria;
+    private String distribucion;
+    private int disponibles;
+    private double precio;
+
+    public LibroTestDataBuilder() {
+        titulo = "Ficciones";
+        categoria = "Literatura";
+        distribucion = "Internacional";
+        disponibles = 3;
+        precio = 45000;
+    }
+
+    public LibroTestDataBuilder conId(Long id) {
+        this.id = id;
+
+        return this;
+    }
+
+    public Libro build() {
+        return new Libro(id, titulo, categoria, distribucion, disponibles, precio);
+    }
 }
