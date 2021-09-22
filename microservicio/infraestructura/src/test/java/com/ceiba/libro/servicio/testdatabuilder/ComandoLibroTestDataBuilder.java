d8466c81fca19b4c215958a94dad72a9340d3120
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-22 03:24:14 -0500

    ComandoLibroTestDataBuilder.java

--- microservicio/infraestructura/src/test/java/com/ceiba/libro/servicio/testdatabuilder/ComandoLibroTestDataBuilder.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/libro/servicio/testdatabuilder/ComandoLibroTestDataBuilder.java
@@ -1,4 +1,32 @@
 package com.ceiba.libro.servicio.testdatabuilder;
 
+import com.ceiba.libro.comando.ComandoLibro;
+import java.util.UUID;
+
 public class ComandoLibroTestDataBuilder {
+
+    private Long id;
+    private String titulo;
+    private String categoria;
+    private String distribucion;
+    private int disponibles;
+    private double precio;
+
+    public ComandoLibroTestDataBuilder() {
+        titulo = UUID.randomUUID().toString();
+        categoria = "Literatura";
+        distribucion = "Internacional";
+        disponibles = 3;
+        precio = 45000;
+    }
+
+    public ComandoLibroTestDataBuilder conTitulo(String titulo) {
+        this.titulo = titulo;
+
+        return this;
+    }
+
+    public ComandoLibro build() {
+        return new ComandoLibro(id, titulo, categoria, distribucion, disponibles, precio);
+    }
 }
