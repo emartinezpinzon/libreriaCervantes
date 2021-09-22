3f063b858976298bf4d431827e5c18e03c382467
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-22 03:23:34 -0500

    Anotacion @Component para FabricaLibro

--- microservicio/aplicacion/src/main/java/com/ceiba/libro/comando/fabrica/FabricaLibro.java
+++ microservicio/aplicacion/src/main/java/com/ceiba/libro/comando/fabrica/FabricaLibro.java
@@ -1,9 +1,13 @@
 package com.ceiba.libro.comando.fabrica;
 
-import com.ceiba.libro.comando.ComandoLibro;
 import com.ceiba.libro.modelo.entidad.Libro;
+import org.springframework.stereotype.Component;
 
+import com.ceiba.libro.comando.ComandoLibro;
+
+@Component
 public class FabricaLibro {
+
     public Libro crear(ComandoLibro comandoLibro) {
         return new Libro(
                 comandoLibro.getId(),
