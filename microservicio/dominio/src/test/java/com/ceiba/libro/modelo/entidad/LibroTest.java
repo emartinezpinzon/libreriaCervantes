89cc995f540faf146f0723027f9b9ed2f9678602
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-22 17:41:10 -0500

    Agrega LibroTest para probar validadores de constructor

---
+++ microservicio/dominio/src/test/java/com/ceiba/libro/modelo/entidad/LibroTest.java
@@ -0,0 +1,11 @@
+package com.ceiba.libro.modelo.entidad;
+
+import com.ceiba.BasePrueba;
+import com.ceiba.dominio.excepcion.ExcepcionValorInvalido;
+import com.ceiba.dominio.excepcion.ExcepcionValorObligatorio;
+import com.ceiba.libro.servicio.testdatabuilder.LibroTestDataBuilder;
+import org.junit.Test;
+
+public class LibroTest {
+
+}
