f65448394f36684aaff4e40dd7e29d95c97b93dc
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-21 22:07:20 -0500

    Agrega servicio actualizar libro

--- microservicio/dominio/src/main/java/com/ceiba/libro/servicio/ServicioActualizarLibro.java
+++ microservicio/dominio/src/main/java/com/ceiba/libro/servicio/ServicioActualizarLibro.java
@@ -1,4 +1,34 @@
 package com.ceiba.libro.servicio;
 
+import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
+import com.ceiba.libro.modelo.entidad.Libro;
+import com.ceiba.libro.puerto.repositorio.RepositorioLibro;
+import com.ceiba.usuario.modelo.entidad.Usuario;
+
 public class ServicioActualizarLibro {
+    private static final String USUARIO_YA_EXISTE = "El usuario ya existe en el sistema";
+
+    private final RepositorioLibro repositorioLibro;
+
+    public ServicioActualizarLibro(RepositorioLibro repositorioLibro) {
+        this.repositorioLibro = repositorioLibro;
+    }
+
+    /**
+     * Ejecuta la actualización de un libro
+     *
+     * @param libro
+     */
+    public void ejecutar(Libro libro) {
+        validarExistenciaPrevia(libro);
+
+        this.repositorioLibro.actualizar(libro);
+    }
+
+    private void validarExistenciaPrevia(Libro libro) {
+        boolean existe = this.repositorioLibro.existeExcluyendoId(libro.getId(), libro.getTitulo());
+
+        if(existe)
+            throw new ExcepcionDuplicidad(USUARIO_YA_EXISTE);
+    }
 }
