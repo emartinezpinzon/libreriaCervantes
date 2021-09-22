5b1c3eb3cca2fa47f696f3ea609876df2551b1b6
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-22 01:21:33 -0500

    MapeoLibro

--- microservicio/infraestructura/src/main/java/com/ceiba/libro/adaptador/dao/MapeoLibro.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/libro/adaptador/dao/MapeoLibro.java
@@ -1,4 +1,23 @@
 package com.ceiba.libro.adaptador.dao;
 
-public class MapeoLibro {
+import com.ceiba.infraestructura.jdbc.MapperResult;
+import com.ceiba.libro.modelo.dto.DtoLibro;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MapeoLibro implements RowMapper<DtoLibro>, MapperResult {
+
+    @Override
+    public DtoLibro mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        Long id = resultSet.getLong("id");
+        String titulo = resultSet.getString("titulo");
+        String categoria = resultSet.getString("categoria");
+        String distribucion = resultSet.getString("distribucion");
+        int disponible = resultSet.getInt("disponible");
+        double precio = resultSet.getDouble("precio");
+
+        return new DtoLibro(id, titulo, categoria, distribucion, disponible, precio);
+    }
 }
