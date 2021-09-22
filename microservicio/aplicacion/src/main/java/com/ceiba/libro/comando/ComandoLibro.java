ac8cbf29a0adc334d61b6816884b7daf5cf3753b
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-22 00:58:22 -0500

    ComandoLibro agregado

--- microservicio/aplicacion/src/main/java/com/ceiba/libro/comando/ComandoLibro.java
+++ microservicio/aplicacion/src/main/java/com/ceiba/libro/comando/ComandoLibro.java
@@ -1,4 +1,20 @@
 package com.ceiba.libro.comando;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ComandoLibro {
+
+    private Long id;
+    private String titulo;
+    private String categoria;
+    private String distribucion;
+    private int disponibles;
+    private double precio;
 }
