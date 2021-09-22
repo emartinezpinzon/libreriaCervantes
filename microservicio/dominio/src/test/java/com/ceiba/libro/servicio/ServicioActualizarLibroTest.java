84ad512754abcc50d03d477076f188328542e910
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-22 00:42:32 -0500

    Test para validar existencia de libro
    [FALLANDO]

--- microservicio/dominio/src/test/java/com/ceiba/libro/servicio/ServicioActualizarLibroTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/libro/servicio/ServicioActualizarLibroTest.java
@@ -1,4 +1,24 @@
 package com.ceiba.libro.servicio;
 
+import com.ceiba.BasePrueba;
+import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
+import com.ceiba.libro.modelo.entidad.Libro;
+import com.ceiba.libro.puerto.repositorio.RepositorioLibro;
+import com.ceiba.libro.servicio.testdatabuilder.LibroTestDataBuilder;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class ServicioActualizarLibroTest {
+    @Test
+    public void validarLibroExistenciaPreviaTest() {
+        // Arrange
+        Libro libro = new LibroTestDataBuilder().conId(1L).build();
+        RepositorioLibro repositorioLibro = Mockito.mock(RepositorioLibro.class);
+        Mockito.when(repositorioLibro.existeExcluyendoId(Mockito.anyLong(), Mockito.anyString())).thenReturn(true);
+        ServicioActualizarLibro servicioActualizarLibro = new ServicioActualizarLibro(repositorioLibro);
+
+        // Act - Assert
+        BasePrueba.assertThrows(() -> servicioActualizarLibro.ejecutar(libro), ExcepcionDuplicidad.class, "Algo");
+    }
+
 }
--- microservicio/dominio/src/test/java/com/ceiba/libro/servicio/ServicioCrearLibroTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/libro/servicio/ServicioCrearLibroTest.java
@@ -1,4 +1,24 @@
 package com.ceiba.libro.servicio;
 
+import com.ceiba.BasePrueba;
+import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
+import com.ceiba.libro.modelo.entidad.Libro;
+import com.ceiba.libro.puerto.repositorio.RepositorioLibro;
+import com.ceiba.libro.servicio.testdatabuilder.LibroTestDataBuilder;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class ServicioCrearLibroTest {
+
+    @Test
+    public void validarLibroExistenciaPreviaTest() {
+        // Arrange
+        Libro libro = new LibroTestDataBuilder().build();
+        RepositorioLibro repositorioLibro = Mockito.mock(RepositorioLibro.class);
+        Mockito.when(repositorioLibro.existe(Mockito.anyString())).thenReturn(true);
+        ServicioCrearLibro servicioCrearLibro = new ServicioCrearLibro(repositorioLibro);
+
+        // Act - Assert
+        BasePrueba.assertThrows(() -> servicioCrearLibro.ejecutar(libro), ExcepcionDuplicidad.class, "El libro ya existe en el sistema");
+    }
 }
