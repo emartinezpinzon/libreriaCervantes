f24db87dc57c8f3cbfc1d221dab48b68782c48fd
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-22 00:58:34 -0500

    Manejadores de crear y actualizar libro creados

--- microservicio/aplicacion/src/main/java/com/ceiba/libro/comando/manejador/ManejadorActualizarLibro.java
+++ microservicio/aplicacion/src/main/java/com/ceiba/libro/comando/manejador/ManejadorActualizarLibro.java
@@ -1,4 +1,26 @@
 package com.ceiba.libro.comando.manejador;
 
-public class ManejadorActualizarLibro {
+import com.ceiba.libro.comando.ComandoLibro;
+import com.ceiba.libro.comando.fabrica.FabricaLibro;
+import com.ceiba.libro.modelo.entidad.Libro;
+import com.ceiba.libro.servicio.ServicioActualizarLibro;
+import com.ceiba.manejador.ManejadorComando;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ManejadorActualizarLibro implements ManejadorComando<ComandoLibro> {
+
+    private final FabricaLibro fabricaLibro;
+    private final ServicioActualizarLibro servicioActualizarLibro;
+
+    public ManejadorActualizarLibro(FabricaLibro fabricaLibro, ServicioActualizarLibro servicioActualizarLibro) {
+        this.fabricaLibro = fabricaLibro;
+        this.servicioActualizarLibro = servicioActualizarLibro;
+    }
+
+    public void ejecutar(ComandoLibro comandoLibro) {
+        Libro libro = this.fabricaLibro.crear(comandoLibro);
+
+        this.servicioActualizarLibro.ejecutar(libro);
+    }
 }
--- microservicio/aplicacion/src/main/java/com/ceiba/libro/comando/manejador/ManejadorCrearLibro.java
+++ microservicio/aplicacion/src/main/java/com/ceiba/libro/comando/manejador/ManejadorCrearLibro.java
@@ -1,4 +1,26 @@
 package com.ceiba.libro.comando.manejador;
 
-public class ManejadorCrearLibro {
+import com.ceiba.ComandoRespuesta;
+import com.ceiba.libro.comando.ComandoLibro;
+import com.ceiba.libro.comando.fabrica.FabricaLibro;
+import com.ceiba.libro.modelo.entidad.Libro;
+import com.ceiba.libro.servicio.ServicioCrearLibro;
+import com.ceiba.manejador.ManejadorComandoRespuesta;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ManejadorCrearLibro implements ManejadorComandoRespuesta<ComandoLibro, ComandoRespuesta<Long>> {
+    private final FabricaLibro fabricaLibro;
+    private final ServicioCrearLibro servicioCrearLibro;
+
+    public ManejadorCrearLibro(FabricaLibro fabricaLibro, ServicioCrearLibro servicioCrearLibro) {
+        this.fabricaLibro = fabricaLibro;
+        this.servicioCrearLibro = servicioCrearLibro;
+    }
+
+    @Override
+    public ComandoRespuesta<Long> ejecutar(ComandoLibro comando) {
+        Libro libro = this.fabricaLibro.crear(comando);
+        return new ComandoRespuesta<>(this.servicioCrearLibro.ejecutar(libro));
+    }
 }
