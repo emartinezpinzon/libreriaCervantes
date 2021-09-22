93813f08523e636401a85863041ede222b3d17a4
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-21 21:29:11 -0500

    Define método obtenerLibros para DaoLibro

--- microservicio/dominio/src/main/java/com/ceiba/libro/puerto/dao/DaoLibro.java
+++ microservicio/dominio/src/main/java/com/ceiba/libro/puerto/dao/DaoLibro.java
@@ -1,4 +1,14 @@
 package com.ceiba.libro.puerto.dao;
 
+import com.ceiba.libro.modelo.dto.DtoLibro;
+
+import java.util.List;
+
 public interface DaoLibro {
+    /**
+     * Permite listar libros
+     *
+     * @return un listado de libros
+     */
+    List<DtoLibro> obtenerLibros();
 }
