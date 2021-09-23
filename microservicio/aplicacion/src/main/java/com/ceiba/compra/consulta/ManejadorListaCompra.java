3c1869571ee551172634bec41c4c9d12de61aa96
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-23 01:07:09 -0500

    ManejadorListaCompra agregado

--- microservicio/aplicacion/src/main/java/com/ceiba/compra/consulta/ManejadorListaCompra.java
+++ microservicio/aplicacion/src/main/java/com/ceiba/compra/consulta/ManejadorListaCompra.java
@@ -1,4 +1,21 @@
 package com.ceiba.compra.consulta;
 
+import com.ceiba.compra.modelo.dto.DtoCompra;
+import com.ceiba.compra.puerto.dao.DaoCompra;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class ManejadorListaCompra {
+
+    private final DaoCompra daoCompra;
+
+    public ManejadorListaCompra(DaoCompra daoCompra) {
+        this.daoCompra = daoCompra;
+    }
+
+    public List<DtoCompra> ejecutar() {
+        return this.daoCompra.listar();
+    }
 }
