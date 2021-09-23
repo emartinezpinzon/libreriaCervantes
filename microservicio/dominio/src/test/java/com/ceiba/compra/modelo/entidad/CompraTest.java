5e574c59ffcd3a929389d7cb2e2fd2784f118df2
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-22 20:26:50 -0500

    Crea el CompraTest para la entidad compra

---
+++ microservicio/dominio/src/test/java/com/ceiba/compra/modelo/entidad/CompraTest.java
@@ -0,0 +1,11 @@
+package com.ceiba.compra.modelo.entidad;
+
+import com.ceiba.BasePrueba;
+import com.ceiba.compra.servicio.testdatabuilder.CompraTestDataBuilder;
+import com.ceiba.dominio.excepcion.ExcepcionValorInvalido;
+import com.ceiba.dominio.excepcion.ExcepcionValorObligatorio;
+import org.junit.Test;
+
+public class CompraTest {
+
+}
