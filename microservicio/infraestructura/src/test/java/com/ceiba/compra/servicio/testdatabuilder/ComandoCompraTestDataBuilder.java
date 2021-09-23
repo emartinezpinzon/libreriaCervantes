8477eb3ee78d1c59999567e9d1eb2af20f23c395
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-23 16:20:40 -0500

    Agrega testDataBuilder para compra

--- microservicio/infraestructura/src/test/java/com/ceiba/compra/servicio/testdatabuilder/ComandoCompraTestDataBuilder.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/compra/servicio/testdatabuilder/ComandoCompraTestDataBuilder.java
@@ -1,4 +1,25 @@
 package com.ceiba.compra.servicio.testdatabuilder;
 
+import com.ceiba.compra.comando.ComandoCompra;
+
 public class ComandoCompraTestDataBuilder {
+
+    private Long id;
+    private Long libroId;
+    private int cantidad;
+
+    public ComandoCompraTestDataBuilder() {
+        libroId = 1L;
+        cantidad = 1;
+    }
+
+    public ComandoCompraTestDataBuilder conCantidadCero() {
+        this.cantidad = 0;
+
+        return this;
+    }
+
+    public ComandoCompra build() {
+        return new ComandoCompra(id, libroId, cantidad);
+    }
 }
