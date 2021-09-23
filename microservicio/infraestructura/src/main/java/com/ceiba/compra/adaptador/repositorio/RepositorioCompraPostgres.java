fd086ce3b7043c2b499125829d982d984a11572e
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-23 04:10:29 -0500

    Agrega la anotación @Repository al RepositorioCompraPostgres.java

--- microservicio/infraestructura/src/main/java/com/ceiba/compra/adaptador/repositorio/RepositorioCompraPostgres.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/compra/adaptador/repositorio/RepositorioCompraPostgres.java
@@ -4,7 +4,9 @@ import com.ceiba.compra.modelo.entidad.Compra;
 import com.ceiba.compra.puerto.repositorio.RepositorioCompra;
 import com.ceiba.infraestructura.jdbc.CustomNamedParameterJdbcTemplate;
 import com.ceiba.infraestructura.jdbc.sqlstatement.SqlStatement;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class RepositorioCompraPostgres implements RepositorioCompra {
 
     private final CustomNamedParameterJdbcTemplate customNamedParameterJdbcTemplate;
