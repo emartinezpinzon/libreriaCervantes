ccd4fd1ebcc05b4ac0738c61435c018044aaedda
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-23 00:18:22 -0500

    ServicioCrearCompraTest.java

---
+++ microservicio/dominio/src/test/java/com/ceiba/compra/servicio/ServicioCrearCompraTest.java
@@ -0,0 +1,17 @@
+package com.ceiba.compra.servicio;
+
+import com.ceiba.BasePrueba;
+import com.ceiba.compra.modelo.entidad.Compra;
+import com.ceiba.compra.puerto.repositorio.RepositorioCompra;
+import com.ceiba.compra.servicio.testdatabuilder.CompraTestDataBuilder;
+import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
+import com.ceiba.dominio.excepcion.ExcepcionLibroNoRegistrado;
+import com.ceiba.libro.modelo.entidad.Libro;
+import com.ceiba.libro.puerto.repositorio.RepositorioLibro;
+import com.ceiba.libro.servicio.testdatabuilder.LibroTestDataBuilder;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class ServicioCrearCompraTest {
+
+}
