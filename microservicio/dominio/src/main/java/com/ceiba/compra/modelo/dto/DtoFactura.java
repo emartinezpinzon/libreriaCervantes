d58870bbb7a98366259a32e9503680993de4d0f8
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-22 23:37:15 -0500

    Creación de achivos para modelo factura

---
+++ microservicio/dominio/src/main/java/com/ceiba/compra/modelo/dto/DtoFactura.java
@@ -0,0 +1,7 @@
+package com.ceiba.compra.modelo.dto;
+
+import java.time.LocalDate;
+
+public class DtoFactura {
+
+}
---
+++ microservicio/dominio/src/main/java/com/ceiba/compra/modelo/entidad/Factura.java
@@ -0,0 +1,4 @@
+package com.ceiba.compra.modelo.entidad;
+
+public class Factura {
+}
