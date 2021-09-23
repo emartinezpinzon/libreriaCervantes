dde0ce1523b01334277db48ca2a4f933343ad993
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-23 16:57:53 -0500

    ConsultaControladorCompraTest agregado test listar
    [FALLANDO]

--- microservicio/infraestructura/src/test/java/com/ceiba/compra/controlador/ConsultaControladorCompraTest.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/compra/controlador/ConsultaControladorCompraTest.java
@@ -1,4 +1,40 @@
 package com.ceiba.compra.controlador;
 
+import com.ceiba.ApplicationMock;
+import com.ceiba.libro.controlador.ConsultaControladorLibro;
+import org.hamcrest.core.IsNull;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = ApplicationMock.class)
+@WebMvcTest(ConsultaControladorLibro.class)
 public class ConsultaControladorCompraTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void listar() throws Exception {
+        // Arrange
+
+        // Act - Assert
+        mockMvc.perform(get("/compra")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id", is("1")));
+    }
 }
