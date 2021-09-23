23008cfa8dccdb4a37c31b50affb6833b9961604
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-23 03:24:05 -0500

    ATENCION | solo agrega una s a disponible[s]
    Revisar si afecta en la serialización y pruebas

--- microservicio/infraestructura/src/main/java/com/ceiba/libro/adaptador/dao/MapeoLibro.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/libro/adaptador/dao/MapeoLibro.java
@@ -15,9 +15,9 @@ public class MapeoLibro implements RowMapper<DtoLibro>, MapperResult {
         String titulo = resultSet.getString("titulo");
         String categoria = resultSet.getString("categoria");
         String distribucion = resultSet.getString("distribucion");
-        int disponible = resultSet.getInt("disponibles");
+        int disponibles = resultSet.getInt("disponibles");
         double precio = resultSet.getDouble("precio");
 
-        return new DtoLibro(id, titulo, categoria, distribucion, disponible, precio);
+        return new DtoLibro(id, titulo, categoria, distribucion, disponibles, precio);
     }
 }
