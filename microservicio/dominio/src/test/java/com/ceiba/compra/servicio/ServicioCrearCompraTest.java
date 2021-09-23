7c92a35d4a2f83c4370acffa52d34508087dd68b
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-23 00:19:57 -0500

    Agrega la validación de no existencia de libro para crear compra

--- microservicio/dominio/src/test/java/com/ceiba/compra/servicio/ServicioCrearCompraTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/compra/servicio/ServicioCrearCompraTest.java
@@ -4,14 +4,23 @@ import com.ceiba.BasePrueba;
 import com.ceiba.compra.modelo.entidad.Compra;
 import com.ceiba.compra.puerto.repositorio.RepositorioCompra;
 import com.ceiba.compra.servicio.testdatabuilder.CompraTestDataBuilder;
-import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
 import com.ceiba.dominio.excepcion.ExcepcionLibroNoRegistrado;
-import com.ceiba.libro.modelo.entidad.Libro;
 import com.ceiba.libro.puerto.repositorio.RepositorioLibro;
-import com.ceiba.libro.servicio.testdatabuilder.LibroTestDataBuilder;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class ServicioCrearCompraTest {
 
+    @Test
+    public void validarLibroNoExistenciaPreviaRegistrarCompraTest() {
+        // Arrange
+        Compra compra = new CompraTestDataBuilder().build();
+        RepositorioCompra repositorioCompra = Mockito.mock(RepositorioCompra.class);
+        RepositorioLibro repositorioLibro = Mockito.mock(RepositorioLibro.class);
+        Mockito.when(repositorioLibro.existe(Mockito.anyLong())).thenReturn(false);
+        ServicioCrearCompra servicioCrearCompra = new ServicioCrearCompra(repositorioCompra, repositorioLibro);
+
+        // Act - Assert
+        BasePrueba.assertThrows(() -> servicioCrearCompra.ejecutar(compra), ExcepcionLibroNoRegistrado.class, "El libro no está registrado");
+    }
 }
