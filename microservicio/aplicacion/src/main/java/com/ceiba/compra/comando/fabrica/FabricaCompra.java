cbbbe334130b3f6edcad06c12d410db0f44952f1
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-23 03:27:13 -0500

    Fabrica de compra

--- microservicio/aplicacion/src/main/java/com/ceiba/compra/comando/fabrica/FabricaCompra.java
+++ microservicio/aplicacion/src/main/java/com/ceiba/compra/comando/fabrica/FabricaCompra.java
@@ -2,7 +2,9 @@ package com.ceiba.compra.comando.fabrica;
 
 import com.ceiba.compra.comando.ComandoCompra;
 import com.ceiba.compra.modelo.entidad.Compra;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FabricaCompra {
 
     public Compra crear(ComandoCompra comandoCompra) {
