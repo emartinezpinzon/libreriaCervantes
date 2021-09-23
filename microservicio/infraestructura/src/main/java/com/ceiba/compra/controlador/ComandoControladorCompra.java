293eb514e5388eac908c1f6c0ac6a436fe0e175b
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-23 03:27:04 -0500

    Controladores de compra (Comando y consulta)

--- microservicio/infraestructura/src/main/java/com/ceiba/compra/controlador/ComandoControladorCompra.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/compra/controlador/ComandoControladorCompra.java
@@ -1,4 +1,31 @@
 package com.ceiba.compra.controlador;
 
+import com.ceiba.ComandoRespuesta;
+import com.ceiba.compra.comando.ComandoCompra;
+import com.ceiba.compra.comando.manejador.ManejadorCrearCompra;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/compra")
+@Api(tags = {"Controlador comando compra"})
 public class ComandoControladorCompra {
+
+    private final ManejadorCrearCompra manejadorCrearCompra;
+
+    @Autowired
+    public ComandoControladorCompra(ManejadorCrearCompra manejadorCrearCompra) {
+        this.manejadorCrearCompra = manejadorCrearCompra;
+    }
+
+    @PostMapping
+    @ApiOperation("Crear compra")
+    public ComandoRespuesta<Long> crear(@RequestBody ComandoCompra comandoCompra) {
+        return manejadorCrearCompra.ejecutar(comandoCompra);
+    }
 }
--- microservicio/infraestructura/src/main/java/com/ceiba/compra/controlador/ConsultaControladorCompra.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/compra/controlador/ConsultaControladorCompra.java
@@ -1,4 +1,29 @@
 package com.ceiba.compra.controlador;
 
+import com.ceiba.compra.consulta.ManejadorListaCompra;
+import com.ceiba.compra.modelo.dto.DtoCompra;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/compra")
+@Api(tags = {"Controlador consulta compra"})
 public class ConsultaControladorCompra {
+
+    private final ManejadorListaCompra manejadorListaCompra;
+
+    public ConsultaControladorCompra(ManejadorListaCompra manejadorListaCompra) {
+        this.manejadorListaCompra = manejadorListaCompra;
+    }
+
+    @GetMapping
+    @ApiOperation("Listar compras")
+    public List<DtoCompra> listar() {
+        return this.manejadorListaCompra.ejecutar();
+    }
 }
