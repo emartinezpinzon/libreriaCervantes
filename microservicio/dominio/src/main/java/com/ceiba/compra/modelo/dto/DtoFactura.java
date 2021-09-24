a2d66c13beaac37ab0c7fdb97e22e40913f18547
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-23 22:15:00 -0500

    Crea los campos para el modelo de Factura

--- microservicio/dominio/src/main/java/com/ceiba/compra/modelo/dto/DtoFactura.java
+++ microservicio/dominio/src/main/java/com/ceiba/compra/modelo/dto/DtoFactura.java
@@ -3,5 +3,7 @@ package com.ceiba.compra.modelo.dto;
 import java.time.LocalDate;
 
 public class DtoFactura {
-
+    private Long id;
+    private double precioFinal;
+    private LocalDate fechaCompra;
 }
--- microservicio/dominio/src/main/java/com/ceiba/compra/modelo/entidad/Factura.java
+++ microservicio/dominio/src/main/java/com/ceiba/compra/modelo/entidad/Factura.java
@@ -1,4 +1,19 @@
 package com.ceiba.compra.modelo.entidad;
 
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
 public class Factura {
+
+    private Long id;
+    private double precioFinal;
+    private LocalDate fechaCompra;
+
+    public Factura(Long id, double precioFinal) {
+        this.id = id;
+        this.precioFinal = precioFinal;
+        this.fechaCompra = LocalDate.now();
+    }
 }
