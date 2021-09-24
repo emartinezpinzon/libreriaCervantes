147215412a9ac89f2e81ba0d63299250e5eb92fe
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-23 21:31:33 -0500

    Consulta el campo fechaEntrega

--- microservicio/infraestructura/src/main/resources/sql/compra/listar.sql
+++ microservicio/infraestructura/src/main/resources/sql/compra/listar.sql
@@ -1,4 +1,4 @@
-select c.id, c.libroId, l.titulo, l.categoria, l.distribucion, l.disponibles, l.precio, c.cantidad
+select c.id, c.libroId, c.fechaEntrega, l.titulo, l.categoria, l.distribucion, l.disponibles, l.precio, c.cantidad
 from compra c
 join libro l
 on c.libroId = l.id
