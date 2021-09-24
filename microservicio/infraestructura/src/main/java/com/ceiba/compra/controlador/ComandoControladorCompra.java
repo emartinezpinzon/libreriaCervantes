1d3ba6629895d6de280a1ab7745c9fa6efe5f2cf
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-23 19:50:49 -0500

    ComandoControladorCompra

--- microservicio/infraestructura/src/main/java/com/ceiba/compra/controlador/ComandoControladorCompra.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/compra/controlador/ComandoControladorCompra.java
@@ -3,13 +3,11 @@ package com.ceiba.compra.controlador;
 import com.ceiba.ComandoRespuesta;
 import com.ceiba.compra.comando.ComandoCompra;
 import com.ceiba.compra.comando.manejador.ManejadorCrearCompra;
+import com.ceiba.compra.comando.manejador.ManejadorActualizarCompra;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/compra")
@@ -17,15 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class ComandoControladorCompra {
 
     private final ManejadorCrearCompra manejadorCrearCompra;
+    private final ManejadorActualizarCompra manejadorActualizarCompra;
 
     @Autowired
-    public ComandoControladorCompra(ManejadorCrearCompra manejadorCrearCompra) {
+    public ComandoControladorCompra(ManejadorCrearCompra manejadorCrearCompra, ManejadorActualizarCompra manejadorActualizarCompra) {
         this.manejadorCrearCompra = manejadorCrearCompra;
+        this.manejadorActualizarCompra = manejadorActualizarCompra;
     }
 
     @PostMapping
     @ApiOperation("Crear compra")
     public ComandoRespuesta<Long> crear(@RequestBody ComandoCompra comandoCompra) {
         return manejadorCrearCompra.ejecutar(comandoCompra);
+    }
+
+
+    @PutMapping(value = "/{id}")
+    @ApiOperation("Actualizar compra")
+    public void actualizar(@RequestBody ComandoCompra comandoCompra, @PathVariable Long id) {
+        comandoCompra.setId(id);
+
+        manejadorActualizarCompra.ejecutar(comandoCompra);
     }
 }
