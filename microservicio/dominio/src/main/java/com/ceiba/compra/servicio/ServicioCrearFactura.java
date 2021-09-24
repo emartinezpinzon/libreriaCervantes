4425df950fa021bfc9aef66d1e9b4165e978e71f
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-24 06:14:06 -0500

    ServicioCrearFactura

--- microservicio/dominio/src/main/java/com/ceiba/compra/servicio/ServicioCrearFactura.java
+++ microservicio/dominio/src/main/java/com/ceiba/compra/servicio/ServicioCrearFactura.java
@@ -1,4 +1,23 @@
 package com.ceiba.compra.servicio;
 
+import com.ceiba.compra.modelo.entidad.Factura;
+import com.ceiba.compra.puerto.repositorio.RepositorioFactura;
+
 public class ServicioCrearFactura {
+
+    private final RepositorioFactura repositorioFactura;
+
+    public ServicioCrearFactura(RepositorioFactura repositorioFactura) {
+        this.repositorioFactura = repositorioFactura;
+    }
+
+    /**
+     * Crea una nueva factura
+     *
+     * @param factura
+     * @return el id de la fa
+     */
+    public Long ejecutar(Factura factura) {
+        return this.repositorioFactura.crear(factura);
+    }
 }
