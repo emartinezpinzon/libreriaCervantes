f0a06c6649dcce8d6890064fa182853b988894cd
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-24 03:27:55 -0500

    Test calcularDescuentosEducacionTest funcionando

--- microservicio/dominio/src/test/java/com/ceiba/compra/servicio/ServicioCalcularPrecioFinalFacturaTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/compra/servicio/ServicioCalcularPrecioFinalFacturaTest.java
@@ -16,4 +16,41 @@ import java.util.List;
 @RunWith(MockitoJUnitRunner.class)
 public class ServicioCalcularPrecioFinalFacturaTest {
 
+    @InjectMocks
+    private ServicioCalcularPrecioFinalFactura servicioCalcularPrecioFinalFactura;
+
+    /**
+     * Calcula el descuento de una compra con dos libros
+     *
+     * Solo debe aplicar el descuento del 20% al libro de educación
+     *
+     * El libro de educación de 100 queda costando 80
+     * El libro de literatura de 20 no recibe descuentos
+     *
+     * La suma sería 80 + 20 = 100
+     */
+    @Test
+    public void calcularDescuentosEducacionTest() {
+        // Arrange
+        List<DtoCompra> compras = new ArrayList<>();
+        DtoCompra libroJava = Mockito.mock(DtoCompra.class);
+        DtoCompra ficciones = Mockito.mock(DtoCompra.class);
+        DaoCompra daoCompra = Mockito.mock(DaoCompra.class);
+
+        // Act
+        Mockito.when(libroJava.getCategoria()).thenReturn("Educación");
+        Mockito.when(libroJava.getPrecio()).thenReturn(100D);
+        compras.add(libroJava);
+
+        Mockito.when(ficciones.getCategoria()).thenReturn("Literatura");
+        Mockito.when(ficciones.getPrecio()).thenReturn(20D);
+        compras.add(ficciones);
+
+        Mockito.when(daoCompra.listar()).thenReturn(compras);
+
+        // Assert
+        Double precioFinal = servicioCalcularPrecioFinalFactura.calcularPrecioFinal(compras);
+        Assert.assertEquals(100D, precioFinal, 1.0);
+    }
+
 }
