779db0d5954674121bb63e3869d86bede5cce8f4
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-23 22:39:59 -0500

    Puerto para factura

--- microservicio/dominio/src/main/java/com/ceiba/compra/puerto/dao/DaoFactura.java
+++ microservicio/dominio/src/main/java/com/ceiba/compra/puerto/dao/DaoFactura.java
@@ -1,4 +1,14 @@
 package com.ceiba.compra.puerto.dao;
 
+import com.ceiba.compra.modelo.dto.DtoFactura;
+
+import java.util.List;
+
 public interface DaoFactura {
+    /**
+     * Permite listar todas las facturas
+     *
+     * @return una lista de facturas
+     */
+    List<DtoFactura> listar();
 }
--- microservicio/dominio/src/main/java/com/ceiba/compra/puerto/repositorio/RepositorioFactura.java
+++ microservicio/dominio/src/main/java/com/ceiba/compra/puerto/repositorio/RepositorioFactura.java
@@ -1,4 +1,13 @@
 package com.ceiba.compra.puerto.repositorio;
 
+import com.ceiba.compra.modelo.entidad.Factura;
+
 public interface RepositorioFactura {
+    /**
+     * Permite agregar una factura
+     *
+     * @param factura
+     * @return el id generado
+     */
+    Long crear(Factura factura);
 }
