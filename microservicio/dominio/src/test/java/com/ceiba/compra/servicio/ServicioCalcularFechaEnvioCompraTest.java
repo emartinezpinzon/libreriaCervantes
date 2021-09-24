17b149e0b506f996617578cbaf49555809577423
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-24 07:24:07 -0500

    Agrega test para fechaEntrega internacional

--- microservicio/dominio/src/test/java/com/ceiba/compra/servicio/ServicioCalcularFechaEnvioCompraTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/compra/servicio/ServicioCalcularFechaEnvioCompraTest.java
@@ -1,4 +1,55 @@
 package com.ceiba.compra.servicio;
 
+import com.ceiba.compra.modelo.dto.DtoCompra;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ServicioCalcularFechaEnvioCompraTest {
+
+    @InjectMocks
+    private ServicioCalcularFechaEnvioCompra servicioCalcularFechaEnvioCompra;
+
+    @Test
+    public void calcularFechaEnvioInternacionalTest() {
+        // Arrange
+        String fecha = "2021-09-24 11:30";
+        String fechaEsperada = "2021-10-01 11:30";
+        DtoCompra compra = Mockito.mock(DtoCompra.class);
+
+        // Act
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime dateTime = LocalDateTime.parse(fecha, formatter);
+        LocalDateTime fechaEntregaEsperada = LocalDateTime.parse(fechaEsperada, formatter);
+        Mockito.when(compra.getDistribucion()).thenReturn("Internacional");
+
+        // Assert
+        LocalDateTime fechaEntrega = servicioCalcularFechaEnvioCompra.calcularFechaEntrega(compra, dateTime);
+        Assert.assertEquals(fechaEntrega, fechaEntregaEsperada);
+    }
+
+    @Test
+    public void calcularFechaEnvioInternacionalIncluidoHoyTest() {
+        // Arrange
+        String fecha = "2021-09-24 07:30";
+        String fechaEsperada = "2021-09-30 07:30";
+        DtoCompra compra = Mockito.mock(DtoCompra.class);
+
+        // Act
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime dateTime = LocalDateTime.parse(fecha, formatter);
+        LocalDateTime fechaEntregaEsperada = LocalDateTime.parse(fechaEsperada, formatter);
+        Mockito.when(compra.getDistribucion()).thenReturn("Internacional");
+
+        // Assert
+        LocalDateTime fechaEntrega = servicioCalcularFechaEnvioCompra.calcularFechaEntrega(compra, dateTime);
+        Assert.assertEquals(fechaEntrega, fechaEntregaEsperada);
+    }
 }
