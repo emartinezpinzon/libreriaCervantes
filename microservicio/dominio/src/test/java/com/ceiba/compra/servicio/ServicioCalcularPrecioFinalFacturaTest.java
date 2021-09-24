8d082e9e822a9335da2b15c51ef666035c95f2d6
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-24 03:27:20 -0500

    Agrega el fichero para Testear el servicio de calcular precio final factura

---
+++ microservicio/dominio/src/test/java/com/ceiba/compra/servicio/ServicioCalcularPrecioFinalFacturaTest.java
@@ -0,0 +1,19 @@
+package com.ceiba.compra.servicio;
+
+import com.ceiba.compra.modelo.dto.DtoCompra;
+import com.ceiba.compra.puerto.dao.DaoCompra;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ServicioCalcularPrecioFinalFacturaTest {
+
+}
