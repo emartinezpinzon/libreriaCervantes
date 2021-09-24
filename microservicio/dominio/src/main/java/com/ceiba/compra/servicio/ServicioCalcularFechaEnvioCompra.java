0c9bb82c6dacf068824762a941d5bbe1bf53fffb
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-24 03:29:39 -0500

    Primeras líneas de ServicioCalcularFechaEnvioCompra

--- microservicio/dominio/src/main/java/com/ceiba/compra/servicio/ServicioCalcularFechaEnvioCompra.java
+++ microservicio/dominio/src/main/java/com/ceiba/compra/servicio/ServicioCalcularFechaEnvioCompra.java
@@ -1,4 +1,15 @@
 package com.ceiba.compra.servicio;
 
+import com.ceiba.compra.puerto.dao.DaoCompra;
+
 public class ServicioCalcularFechaEnvioCompra {
+
+    private static final String DISTRIBUCION_NACIONAL = "Nacional";
+    private static final String DISTRIBUCION_INTERNACIONAL = "Internacional";
+
+    private final DaoCompra daoCompra;
+
+    public ServicioCalcularFechaEnvioCompra(DaoCompra daoCompra) {
+        this.daoCompra = daoCompra;
+    }
 }
