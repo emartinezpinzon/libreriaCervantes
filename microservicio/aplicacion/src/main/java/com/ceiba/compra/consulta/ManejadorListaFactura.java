0ae2170483caf3a88bd3f9c17f05cf1142764b5e
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-27 13:21:15 -0500

    Completa el ManejadorListaFactura

--- microservicio/aplicacion/src/main/java/com/ceiba/compra/consulta/ManejadorListaFactura.java
+++ microservicio/aplicacion/src/main/java/com/ceiba/compra/consulta/ManejadorListaFactura.java
@@ -1,4 +1,21 @@
 package com.ceiba.compra.consulta;
 
+import com.ceiba.compra.modelo.dto.DtoFactura;
+import com.ceiba.compra.puerto.dao.DaoFactura;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class ManejadorListaFactura {
+
+    private final DaoFactura daoFactura;
+
+    public ManejadorListaFactura(DaoFactura daoFactura) {
+        this.daoFactura = daoFactura;
+    }
+
+    public List<DtoFactura> ejecutar() {
+        return this.daoFactura.listar();
+    }
 }
