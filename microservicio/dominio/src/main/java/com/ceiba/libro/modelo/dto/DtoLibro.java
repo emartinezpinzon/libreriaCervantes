e884baa5f68185eddce92dd9e922d0e094fe8700
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-27 01:20:24 -0500

    Agrega la anotación @Setter para DtoLibro

--- microservicio/dominio/src/main/java/com/ceiba/libro/modelo/dto/DtoLibro.java
+++ microservicio/dominio/src/main/java/com/ceiba/libro/modelo/dto/DtoLibro.java
@@ -2,8 +2,10 @@ package com.ceiba.libro.modelo.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class DtoLibro {
     private Long id;
