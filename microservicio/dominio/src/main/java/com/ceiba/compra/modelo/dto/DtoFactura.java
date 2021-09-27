0b3c3dbe77807bbefd6a6d200ddacc26a5f17f48
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-27 15:51:57 -0500

    Agrega el mapeo de factura

--- microservicio/dominio/src/main/java/com/ceiba/compra/modelo/dto/DtoFactura.java
+++ microservicio/dominio/src/main/java/com/ceiba/compra/modelo/dto/DtoFactura.java
@@ -1,7 +1,12 @@
 package com.ceiba.compra.modelo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.time.LocalDate;
 
+@Getter
+@AllArgsConstructor
 public class DtoFactura {
     private Long id;
     private double precioFinal;
--- microservicio/infraestructura/src/main/java/com/ceiba/compra/adaptador/dao/MapeoFactura.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/compra/adaptador/dao/MapeoFactura.java
@@ -1,4 +1,21 @@
 package com.ceiba.compra.adaptador.dao;
 
-public class MapeoFactura {
+import com.ceiba.compra.modelo.dto.DtoFactura;
+import com.ceiba.infraestructura.jdbc.MapperResult;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class MapeoFactura implements RowMapper<DtoFactura>, MapperResult {
+
+    @Override
+    public DtoFactura mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        Long id = resultSet.getLong("id");
+        double precioFinal = resultSet.getDouble("precioFinal");
+        LocalDate fechaCompra = extraerLocalDate(resultSet, "fechaCompra");
+
+        return new DtoFactura(id, precioFinal, fechaCompra);
+    }
 }
