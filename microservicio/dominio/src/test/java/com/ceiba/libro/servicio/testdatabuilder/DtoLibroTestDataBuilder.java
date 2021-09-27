77b59a6945638beca9fe8e900703024061ab1588
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-27 01:20:09 -0500

    Implementa un DtoLibroTestDataBuilder para las pruebas del ServicioCrearCompraTest

--- microservicio/dominio/src/test/java/com/ceiba/libro/servicio/testdatabuilder/DtoLibroTestDataBuilder.java
+++ microservicio/dominio/src/test/java/com/ceiba/libro/servicio/testdatabuilder/DtoLibroTestDataBuilder.java
@@ -1,4 +1,26 @@
 package com.ceiba.libro.servicio.testdatabuilder;
 
+import com.ceiba.libro.modelo.dto.DtoLibro;
+
+import java.util.UUID;
+
 public class DtoLibroTestDataBuilder {
+    private Long id;
+    private String titulo;
+    private String categoria;
+    private String distribucion;
+    private int disponibles;
+    private double precio;
+
+    public DtoLibroTestDataBuilder() {
+        titulo = UUID.randomUUID().toString();
+        categoria = UUID.randomUUID().toString();
+        distribucion = UUID.randomUUID().toString();
+        disponibles = 3;
+        precio = 45000D;
+    }
+
+    public DtoLibro build() {
+        return new DtoLibro(id, titulo, categoria, distribucion, disponibles, precio);
+    }
 }
