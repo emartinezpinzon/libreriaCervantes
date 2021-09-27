11b19a0429302d64b4da90b89968c002da2c6d9a
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-27 13:11:17 -0500

    Agrega validar existencia de libros en compra

--- microservicio/dominio/src/main/java/com/ceiba/compra/servicio/ServicioCrearFactura.java
+++ microservicio/dominio/src/main/java/com/ceiba/compra/servicio/ServicioCrearFactura.java
@@ -1,23 +1,33 @@
 package com.ceiba.compra.servicio;
 
 import com.ceiba.compra.modelo.entidad.Factura;
+import com.ceiba.compra.puerto.dao.DaoCompra;
 import com.ceiba.compra.puerto.repositorio.RepositorioFactura;
+import com.ceiba.dominio.excepcion.ExcepcionSinDatos;
 
 public class ServicioCrearFactura {
 
-    private final RepositorioFactura repositorioFactura;
+    private static final String COMPRAS_NO_REGISTRADAS = "No hay compras registradas";
 
-    public ServicioCrearFactura(RepositorioFactura repositorioFactura) {
+    private final RepositorioFactura repositorioFactura;
+    private final DaoCompra daoCompra;
+
+    public ServicioCrearFactura(RepositorioFactura repositorioFactura,
+                                DaoCompra daoCompra) {
         this.repositorioFactura = repositorioFactura;
+        this.daoCompra = daoCompra;
     }
 
-    /**
-     * Crea una nueva factura
-     *
-     * @param factura
-     * @return el id de la fa
-     */
     public Long ejecutar(Factura factura) {
+        validarExistenciaLibrosEnCompra();
+
         return this.repositorioFactura.crear(factura);
+    }
+
+    private void validarExistenciaLibrosEnCompra() {
+        int compras = daoCompra.listar().size();
+
+        if (compras == 0)
+            throw new ExcepcionSinDatos(COMPRAS_NO_REGISTRADAS);
     }
 }
