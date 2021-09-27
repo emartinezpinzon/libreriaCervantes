393639c57377c82c5b7ab2d3770f43f077c99868
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-26 23:50:02 -0500

    Usa las anotaciones @Mock e @InjectMock para hacer más legible el código entre las pruebas que se agregarán

--- microservicio/dominio/src/test/java/com/ceiba/compra/servicio/ServicioCrearCompraTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/compra/servicio/ServicioCrearCompraTest.java
@@ -5,20 +5,39 @@ import com.ceiba.compra.modelo.entidad.Compra;
 import com.ceiba.compra.puerto.repositorio.RepositorioCompra;
 import com.ceiba.compra.servicio.testdatabuilder.CompraTestDataBuilder;
 import com.ceiba.dominio.excepcion.ExcepcionLibroNoRegistrado;
+import com.ceiba.libro.modelo.dto.DtoLibro;
+import com.ceiba.libro.puerto.dao.DaoLibro;
 import com.ceiba.libro.puerto.repositorio.RepositorioLibro;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.UUID;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ServicioCrearCompraTest {
+
+    @Mock
+    private DaoLibro libro;
+
+    @Mock
+    private RepositorioCompra repositorioCompra;
+
+    @Mock
+    private RepositorioLibro repositorioLibro;
+
+    @InjectMocks
+    Compra compra = new CompraTestDataBuilder().build();
 
     @Test
     public void validarLibroNoExistenciaPreviaRegistrarCompraTest() {
         // Arrange
-        Compra compra = new CompraTestDataBuilder().build();
-        RepositorioCompra repositorioCompra = Mockito.mock(RepositorioCompra.class);
-        RepositorioLibro repositorioLibro = Mockito.mock(RepositorioLibro.class);
         Mockito.when(repositorioLibro.existe(Mockito.anyLong())).thenReturn(false);
-        ServicioCrearCompra servicioCrearCompra = new ServicioCrearCompra(repositorioCompra, repositorioLibro);
+        ServicioCrearCompra servicioCrearCompra = new ServicioCrearCompra(repositorioCompra, repositorioLibro, libro);
 
         // Act - Assert
         BasePrueba.assertThrows(() -> servicioCrearCompra.ejecutar(compra), ExcepcionLibroNoRegistrado.class, "El libro no está registrado");
