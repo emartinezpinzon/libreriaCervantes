073a26d9e957944658478e74ec8a633fd2e5cc21
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-27 15:58:28 -0500

    Implementa en DaoFacturaPostgres el método para listar las facturas creadas

--- microservicio/infraestructura/src/main/java/com/ceiba/compra/adaptador/dao/DaoFacturaPostgres.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/compra/adaptador/dao/DaoFacturaPostgres.java
@@ -1,4 +1,27 @@
 package com.ceiba.compra.adaptador.dao;
 
-public class DaoFacturaPostgres {
+import com.ceiba.compra.modelo.dto.DtoFactura;
+import com.ceiba.compra.puerto.dao.DaoFactura;
+import com.ceiba.infraestructura.jdbc.CustomNamedParameterJdbcTemplate;
+import com.ceiba.infraestructura.jdbc.sqlstatement.SqlStatement;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DaoFacturaPostgres implements DaoFactura {
+
+    private final CustomNamedParameterJdbcTemplate customNamedParameterJdbcTemplate;
+
+    @SqlStatement(namespace = "factura", value = "listar")
+    private static String sqlListar;
+
+    public DaoFacturaPostgres(CustomNamedParameterJdbcTemplate customNamedParameterJdbcTemplate) {
+        this.customNamedParameterJdbcTemplate = customNamedParameterJdbcTemplate;
+    }
+
+    @Override
+    public List<DtoFactura> listar() {
+        return this.customNamedParameterJdbcTemplate.getNamedParameterJdbcTemplate().query(sqlListar, new MapeoFactura());
+    }
 }
