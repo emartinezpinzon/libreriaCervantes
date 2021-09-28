65f6f1d2dce38cbb8556ea031d598a72fa027b02
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-27 21:30:48 -0500

    Completa FacturaTestDataBuilder

--- microservicio/dominio/src/test/java/com/ceiba/compra/servicio/testdatabuilder/FacturaTestDataBuilder.java
+++ microservicio/dominio/src/test/java/com/ceiba/compra/servicio/testdatabuilder/FacturaTestDataBuilder.java
@@ -1,4 +1,18 @@
 package com.ceiba.compra.servicio.testdatabuilder;
 
+import com.ceiba.compra.modelo.entidad.Factura;
+
+import java.time.LocalDate;
+
 public class FacturaTestDataBuilder {
+    private Long id;
+    private double precioFinal;
+    private LocalDate fechaCompra;
+
+    public FacturaTestDataBuilder() {
+    }
+
+    public Factura build() {
+        return new Factura(id, precioFinal, fechaCompra);
+    }
 }
