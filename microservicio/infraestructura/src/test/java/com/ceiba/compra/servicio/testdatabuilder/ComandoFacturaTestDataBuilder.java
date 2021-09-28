a372c0c8d511a845a9a360fdb8818cb077424100
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-27 19:50:27 -0500

    Agrega el test data builder para comando factura

--- microservicio/infraestructura/src/test/java/com/ceiba/compra/servicio/testdatabuilder/ComandoFacturaTestDataBuilder.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/compra/servicio/testdatabuilder/ComandoFacturaTestDataBuilder.java
@@ -1,4 +1,19 @@
 package com.ceiba.compra.servicio.testdatabuilder;
 
+import com.ceiba.compra.comando.ComandoFactura;
+
+import java.time.LocalDate;
+
 public class ComandoFacturaTestDataBuilder {
+
+    private Long id;
+    private double precioFinal;
+    private LocalDate fechaCompra;
+
+    public ComandoFacturaTestDataBuilder() {
+    }
+
+    public ComandoFactura build() {
+        return new ComandoFactura(id, precioFinal, fechaCompra);
+    }
 }
