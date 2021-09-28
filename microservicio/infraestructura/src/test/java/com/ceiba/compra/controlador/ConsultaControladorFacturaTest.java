e1b2f679d87cbacf43c0a12c0e4986d8491408e9
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-27 21:56:42 -0500

    Test completo para ConsultaControladorFactura

--- microservicio/infraestructura/src/test/java/com/ceiba/compra/controlador/ConsultaControladorFacturaTest.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/compra/controlador/ConsultaControladorFacturaTest.java
@@ -1,4 +1,39 @@
 package com.ceiba.compra.controlador;
 
+import com.ceiba.ApplicationMock;
+import org.hamcrest.core.IsNull;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = ApplicationMock.class)
+@WebMvcTest(ConsultaControladorFactura.class)
 public class ConsultaControladorFacturaTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void listar() throws Exception {
+        // Arrange
+
+        // Act - Assert
+        mockMvc.perform(get("/factura")
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$", hasSize(2)))
+                        .andExpect(jsonPath("$[0].id", IsNull.notNullValue()))
+                        .andExpect(jsonPath("$[0].precioFinal", IsNull.notNullValue()));
+    }
 }
