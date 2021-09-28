f83d950071cd914444c8dca9fd400b5d4c230af7
Emanuel Martinez <emartinezpinzon@gmail.com>
2021-09-28 02:37:00 -0500

    Test completos para Servicio actualizar libro

--- microservicio/dominio/src/test/java/com/ceiba/libro/servicio/ServicioActualizarLibroTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/libro/servicio/ServicioActualizarLibroTest.java
@@ -7,9 +7,14 @@ import com.ceiba.libro.modelo.entidad.Libro;
 import com.ceiba.libro.puerto.repositorio.RepositorioLibro;
 import com.ceiba.libro.servicio.testdatabuilder.LibroTestDataBuilder;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
 public class ServicioActualizarLibroTest {
+
+    private static final String LIBRO_YA_EXISTE = "El libro ya existe en el sistema";
 
     @Test
     public void validarLibroExistenciaPreviaTest() {
@@ -20,7 +25,21 @@ public class ServicioActualizarLibroTest {
         ServicioActualizarLibro servicioActualizarLibro = new ServicioActualizarLibro(repositorioLibro);
 
         // Act - Assert
-        BasePrueba.assertThrows(() -> servicioActualizarLibro.ejecutar(libro), ExcepcionDuplicidad.class, "El libro ya existe en el sistema");
+        BasePrueba.assertThrows(() -> servicioActualizarLibro.ejecutar(libro), ExcepcionDuplicidad.class, LIBRO_YA_EXISTE);
+    }
+
+    // Unit Test para actualizar
+    @Test
+    public void validarLibroNoExistenciaPreviaTest() {
+        // Arrange
+        Libro libro = new LibroTestDataBuilder().conId(22L).build();
+        RepositorioLibro repositorioLibro = Mockito.mock(RepositorioLibro.class);
+        Mockito.when(repositorioLibro.existeExcluyendoId(Mockito.anyLong(), Mockito.anyString())).thenReturn(false);
+        ServicioActualizarLibro servicioActualizarLibro = new ServicioActualizarLibro(repositorioLibro);
+
+        // Act - Assert
+        Assertions.assertDoesNotThrow(() -> servicioActualizarLibro.ejecutar(libro));
+        Mockito.verify(repositorioLibro, Mockito.times(1)).actualizar(libro);
     }
 
 }
